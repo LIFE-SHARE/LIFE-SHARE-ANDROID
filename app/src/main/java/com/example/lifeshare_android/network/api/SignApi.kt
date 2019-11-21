7bdd5e2c269f7ef678a8456e1473b1b32e3887b4
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-11-21 21:24:07 +0900

    Update SignApi.kt

--- app/src/main/java/com/example/lifeshare_android/network/api/SignApi.kt
+++ app/src/main/java/com/example/lifeshare_android/network/api/SignApi.kt
@@ -1,7 +1,5 @@
 package com.example.lifeshare_android.network.api
 
-import com.example.lifeshare_android.model.user.Member
-
 import com.example.lifeshare_android.network.request.LoginRequest
 import com.example.lifeshare_android.network.response.Response
 import com.example.lifeshare_android.network.response.data.LoginData
@@ -9,14 +7,10 @@ import com.example.lifeshare_android.network.response.data.LoginData
 import io.reactivex.Single
 
 import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface SignApi {
 
     @POST("/auth/login")
     fun login(@Body loginRequest: LoginRequest): Single<retrofit2.Response<Response<LoginData>>>
-
-    @POST("/auth/autoLogin")
-    fun autoLogin(@Header("Authorization") token: String): Single<retrofit2.Response<Response<Member>>>
 }

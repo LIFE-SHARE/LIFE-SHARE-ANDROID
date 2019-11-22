ae99acd49df822cbe6fd75effdeff6abcdd6d2d5
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-11-22 11:02:47 +0900

    Update Strings.kt

--- app/src/main/java/com/example/lifeshare_android/util/Strings.kt
+++ app/src/main/java/com/example/lifeshare_android/util/Strings.kt
@@ -2,5 +2,7 @@ package com.example.lifeshare_android.util
 
 object Strings {
 
-    const val MAIN_HOST = "http://172.26.0.54:8000"
+    const val MAIN_HOST = "http://192.168.1.12:8000"
+
+    // TODO : 172.26.0.54
 }

3eb4fcc2a7474c636a66395531bef3db6be2190e
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-11-24 22:29:53 +0900

    set

--- app/src/main/java/com/example/lifeshare_android/view/activity/AddHouseActivity2.kt
+++ app/src/main/java/com/example/lifeshare_android/view/activity/AddHouseActivity2.kt
@@ -1,24 +1,47 @@
 package com.example.lifeshare_android.view.activity
 
+import android.content.Intent
+import androidx.lifecycle.Observer
+
+import com.example.lifeshare_android.BR
+import com.example.lifeshare_android.R
 import com.example.lifeshare_android.base.activity.BaseActivity
+import com.example.lifeshare_android.base.activity.BasePictureActivity
 import com.example.lifeshare_android.databinding.ActivityAddHouseBinding
 import com.example.lifeshare_android.viewmodel.AddHouseViewModel
 
-class AddHouseActivity2 : BaseActivity<ActivityAddHouseBinding, AddHouseViewModel>() {
+class AddHouseActivity2 : BasePictureActivity<ActivityAddHouseBinding, AddHouseViewModel>() {
 
     override fun getLayoutId(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return R.layout.activity_add_house
     }
 
     override fun getViewModel(): Class<AddHouseViewModel> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return AddHouseViewModel::class.java
     }
 
     override fun getBindingVariable(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return BR.viewModel
     }
 
     override fun initObserver() {
+        with(viewModel) {
+
+            addImageEvent.observe(this@AddHouseActivity2, Observer {
+                goToAlbum()
+            })
+        }
+    }
+
+    override fun requestNotOkEvent() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pickNextEvent(data: Intent) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun cropNextEvent() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/com/example/lifeshare_android/viewmodel/AddHouseViewModel.kt
+++ app/src/main/java/com/example/lifeshare_android/viewmodel/AddHouseViewModel.kt
@@ -3,8 +3,15 @@ package com.example.lifeshare_android.viewmodel
 import android.app.Application
 
 import com.example.lifeshare_android.base.viewmodel.BaseViewModel
+import com.example.lifeshare_android.widget.SingleLiveEvent
 
 class AddHouseViewModel(application: Application) : BaseViewModel<Any>(application) {
+
+    val addImageEvent = SingleLiveEvent<Unit>()
+
+    fun addInputImageBtn() {
+        addImageEvent.call()
+    }
 
     override fun onRetrieveDataSuccess(data: Any) {}
 

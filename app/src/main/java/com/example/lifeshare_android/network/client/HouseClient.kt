8868f118790729f0fb18d55b225f164eca75a0be
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-11-25 09:34:44 +0900

    Update HouseClient.kt

--- app/src/main/java/com/example/lifeshare_android/network/client/HouseClient.kt
+++ app/src/main/java/com/example/lifeshare_android/network/client/HouseClient.kt
@@ -2,10 +2,14 @@ package com.example.lifeshare_android.network.client
 
 import com.example.lifeshare_android.base.BaseClient
 import com.example.lifeshare_android.network.api.HouseApi
+import com.example.lifeshare_android.network.response.Response
 import com.example.lifeshare_android.network.response.data.HouseData
 import com.example.lifeshare_android.network.response.data.HouseDatas
 
 import io.reactivex.Single
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.Header
 
 class HouseClient : BaseClient<HouseApi>() {
 

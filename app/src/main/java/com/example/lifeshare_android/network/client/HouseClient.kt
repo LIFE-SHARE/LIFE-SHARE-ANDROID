b8902805c88f72bf161c67c5a664da8f5c918eda
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-11-25 12:58:30 +0900

    Update HouseClient.kt

--- app/src/main/java/com/example/lifeshare_android/network/client/HouseClient.kt
+++ app/src/main/java/com/example/lifeshare_android/network/client/HouseClient.kt
@@ -2,14 +2,13 @@ package com.example.lifeshare_android.network.client
 
 import com.example.lifeshare_android.base.BaseClient
 import com.example.lifeshare_android.network.api.HouseApi
-import com.example.lifeshare_android.network.response.Response
 import com.example.lifeshare_android.network.response.data.HouseData
 import com.example.lifeshare_android.network.response.data.HouseDatas
 
 import io.reactivex.Single
+
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.http.Header
 
 class HouseClient : BaseClient<HouseApi>() {
 
@@ -19,6 +18,18 @@ class HouseClient : BaseClient<HouseApi>() {
 
     fun getAllHouse(token: String): Single<HouseDatas> {
         return api.getAllHouse(token).map(getResponseObjectsFunction())
+    }
+
+    fun addPostHouse(token: String,
+                     maxMember: RequestBody,
+                     name: RequestBody,
+                     address: RequestBody,
+                     genderLimit: RequestBody,
+                     ageLimit: RequestBody,
+                     contractperiod: RequestBody,
+                     information: RequestBody,
+                     image: MultipartBody.Part): Single<Any> {
+        return api.postHouse(token, maxMember, name, address, genderLimit, ageLimit, contractperiod, information, image).map(getResponseMessageFunction())
     }
 
     override fun type(): Class<HouseApi> {
